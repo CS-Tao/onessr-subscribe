3c7fa45db8336c2e7a89493db08859ca92a8d9fc
CS-Tao <990296951@qq.com>
2019-11-17 09:20:15 +0800

    Log last update time

---
+++ public/js/index.js
@@ -0,0 +1,24 @@
+function getLastUpdateTime (url, elementId) {
+  var data = null;
+
+  var xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
+
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) {
+      document.getElementById(elementId).innerText = 'Last update: ' + (this.responseText ? this.responseText.replace('\n', '') : 'Error to fetch data');
+    }
+  });
+
+  xhr.open("GET", url);
+  xhr.setRequestHeader("Accept", "*/*");
+  xhr.setRequestHeader("Cache-Control", "no-cache");
+  xhr.setRequestHeader("cache-control", "no-cache");
+  xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+
+  xhr.send(data);
+}
+
+window.onload = function () {
+  getLastUpdateTime('https://home.cs-tao.cc/onessr-subscribe/last-update.txt', 'last-update')
+}
