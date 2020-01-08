3720d78a091005100f0b4bd1ce6065d2623606be
CS-Tao <990296951@qq.com>
2020-01-09 01:22:54 +0800

    Reuse ssr

--- README.md
+++ README.md
@@ -1,32 +1,5 @@
-[![Build status](https://github.com/CS-Tao/onessr-subscribe/workflows/schedule/badge.svg)](https://github.com/CS-Tao/onessr-subscribe/actions?query=workflow%3Aschedule)
 # onessr-subscribe
 
-> 曲线爱国第一步
+> 同心共筑中国梦
 
-```javascript
-/** 网站声明
- * @method statement
- * @param {Person} you 你
- * @return {boolean} 是否希望你使用
-*/
-function statement (you) {
-  if (you.isLoveChina) {
-    if (you.knownHowToUse) {
-      switch (you.getPurpose()) {
-        case '使用墙外社交软件':
-        case '抵制墙外瞎报道的媒体':
-        case '做任何爱国举动':
-          return true
-        default:
-          throw Error('请勿使用')
-      }
-    } else {
-      return false
-    }
-  } else {
-    throw Error('戈乌嗯')
-  }
-}
-
-statement(window.getCurrentGuest())
-```
+[![Build status](https://github.com/CS-Tao/onessr-subscribe/workflows/schedule/badge.svg)](https://github.com/CS-Tao/onessr-subscribe/actions?query=workflow%3Aschedule)
--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-const fs = require('fs')
-const path = require('path')
-const { observe } = require('./observer')
-const meta = require('./meta')
-const OUTPUT_DIR = 'public'
+var unirest = require("unirest");
+const path = require("path")
+const fs = require("fs")
 
-function convert (content) {
-  return content.replace(/<p>/g, '').replace(/<\/p>/g, '\n')
+function observe (cb) {
+  var req = unirest("GET", 'https://jichangdaquan.com/node/429.html');
+
+  req.headers({
+    "cache-control": "no-cache",
+    "Connection": "keep-alive",
+    "Accept-Encoding": "gzip, deflate",
+    "Cache-Control": "no-cache",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.29 Safari/537.36 Edg/79.0.309.18",
+    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "accept-language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+    "acept-encoding": "gzip, deflate, br",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+  });
+
+  req.end(function (res) {
+    if (res.error) throw new Error(res.error);
+    cb(res.body);
+  });
 }
 
+
 function cb (data) {
-  var result = Buffer.from(convert(data[0].articleContent)).toString('base64')
-  var filePath = path.resolve(__dirname, OUTPUT_DIR, 'onessr-sub.txt')
+  const front = /data-clipboard-text="/
+  const back = /">点击复制全部ssr节点/
+  var ssrNodes = data.split(front)[1].split(back)[0]
+  var result = Buffer.from(ssrNodes).toString('base64')
+  var filePath = path.resolve(__dirname, 'public', 'onessr-sub.txt')
   fs.writeFile(filePath, result, function (e) {
     if (e) {
       throw new Error(e)
@@ -20,4 +39,4 @@ function cb (data) {
   })
 }
 
-observe(meta.api, meta.options, cb)
+observe(cb)
